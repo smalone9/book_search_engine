58156e48a67b52c7cfcb46371e2e785780704586
Samantha Malone <sammac99@gmail.com>
2021-09-26 10:54:03 -0700

    added queries and front-end files

---
+++ client/src/utils/queries.js
@@ -0,0 +1,20 @@
+import gql from "graphql-tag";
+
+export const GET_M = gql`
+  {
+    me {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
